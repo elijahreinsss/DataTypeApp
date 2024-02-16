d44d55d1d659298924afb200446253981d49b297
santos.283553 <santos.283553@LAB-B-PC35>
2024-02-16 09:24:25 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+
+namespace DataTypeApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the pieces of apple :");
+            int apples = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter total price of " + apples + " apple(s) : " );
+            double price = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The total price of " + apples + " apple(s) :" + price);
+
+            int converted_price = (int)price;
+            Console.WriteLine("The value of original price is : " + price);
+            Console.WriteLine("The value of converted price is :" + converted_price);
+            Console.WriteLine("Press any key to quit");
+            Console.ReadKey();
+        }
+    }
+}
